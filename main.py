0bd92ccac2d85de2cb4152fc993548a9e1e079cb
wilowkqxy <wilowkqxy@gmail.com>
2025-08-08 22:50:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,71 @@
 #!/usr/bin/python3
 
-import telebot
-import requests
-import random
+import sys
 
-from telebot import types
-from datetime import datetime, timezone
+try:
+	import telebot
+	import requests
+	import random
+	import json
+	import threading
 
-from lang import *
-from supersecret import *
+	from telebot import types
+	from datetime import datetime, timezone
+
+	from lang import *
+	from supersecret import *
+except Exception as e:
+	print(f"FATAL ERROR WHILE IMPORTING: {str(e)}")
+	sys.exit(1)
 
 model_ai = "gemini-2.5-flash"
 GOOGLE_GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{model_ai}:generateContent"
 
-users = {}
-
-aiMode = []
-
 back_markup = types.InlineKeyboardMarkup()
 back_markup.add(types.InlineKeyboardButton(text="↩️ назад",callback_data="back"))
+
+users = {}
+aiMode = []
+
+inAiChat = []
+
+#dataLoadFailed = False
+
+#try:
+#	with open("data.json","r") as data:
+#		userdata = json.load(data)
+#
+#		users = userdata.get("users",{})
+#		aiMode = userdata.get("aiMode",[])
+#except Exception as e:
+#	dataLoadFailed = True
+#	print(f"failed to load userdata, loading with default values: {str(e)}")
+#	users = {}
+#	aiMode = []
+
+#print(users)
+#print(aiMode)
+
+#def autoSave():
+#	print("auto saving...")
+#	try:
+#		if dataLoadFailed:
+#			return
+#		else:
+#			with open("data.json","w") as data:
+#				userdata = {
+#					"users": users,
+#					"aiMode": aiMode
+#				}
+#
+#				json.dump(userdata,data)
+#
+#			threading.Timer(30,autoSave).start()
+#
+#	except Exception as e:
+#		print(f"failed to save data: {str(e)}")
+
+#autoSave()
 
 bot = telebot.TeleBot(SUPERSECRETTOKENLOL)
 
@@ -49,74 +96,114 @@ def getAIResponse(prompt):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-	if call.data == "discord":
-		if call.from_user.id in users.keys():
-			bot.edit_message_text(chat_id=call.from_user.id,text=discord_msg,message_id=users[call.from_user.id],reply_markup=back_markup)
-			return
-		bot.send_message(call.from_user.id,discord_msg,reply_markup=back_markup)
-	elif call.data == "tg":
-		if call.from_user.id in users.keys():
-			bot.edit_message_text(chat_id=call.from_user.id,text=tg_msg,message_id=users[call.from_user.id],reply_markup=back_markup)
-			return
-		bot.send_message(call.from_user.id,tg_msg,reply_markup=back_markup)
-	elif call.data == "about":
-		if call.from_user.id in users.keys():
-			bot.edit_message_text(chat_id=call.from_user.id,text=about_msg,message_id=users[call.from_user.id],reply_markup=back_markup)
-			return
-	elif call.data == "ai":
-		if call.from_user.id in users.keys():
-			if call.from_user.id not in aiMode:
-				aiMode.append(call.from_user.id)
-			bot.edit_message_text(chat_id=call.from_user.id,text=ai_prompt_msg,message_id=users[call.from_user.id],reply_markup=back_markup)
-			return
-	elif call.data == "back":
-		if call.from_user.id in aiMode:
-			aiMode.remove(call.from_user.id)
+	with open("assets/starter.png","rb") as pic:
+		if call.data == "discord":
+			if call.from_user.id in users.keys():
+				bot.edit_message_caption(chat_id=call.from_user.id,message_id=users[call.from_user.id],caption=discord_msg,reply_markup=back_markup)
+				return
+			else:
+				bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=discord_msg,reply_markup=back_markup)
+				users[call.from_user.id] = botmsg.message_id
+		elif call.data == "tg":
+			if call.from_user.id in users.keys():
+				#bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				#botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=tg_msg,reply_markup=back_markup)
+				#users[call.from_user.id] = botmsg.message_id
+				bot.edit_message_caption(chat_id=call.from_user.id,message_id=users[call.from_user.id],caption=tg_msg,reply_markup=back_markup)
+				return
+			else:
+				bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=tg_msg,reply_markup=back_markup)
+				users[call.from_user.id] = botmsg.message_id
+		elif call.data == "about":
+			if call.from_user.id in users.keys():
+				#bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				#botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=about_msg,reply_markup=back_markup)
+				#users[call.from_user.id] = botmsg.message_id
+				bot.edit_message_caption(chat_id=call.from_user.id,message_id=users[call.from_user.id],caption=about_msg,reply_markup=back_markup)
+				return
+			else:
+				bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=about_msg,reply_markup=back_markup)
+				users[call.from_user.id] = botmsg.message_id
+		elif call.data == "ai":
+			if call.from_user.id in users.keys():
+				if call.from_user.id not in aiMode:
+					aiMode.append(call.from_user.id)
+				#bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				#botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=ai_prompt_msg,reply_markup=back_markup)
+				#users[call.from_user.id] = botmsg.message_id
+				bot.edit_message_caption(chat_id=call.from_user.id,message_id=users[call.from_user.id],caption=ai_prompt_msg,reply_markup=back_markup)
+				return
+			else:
+				if call.from_user.id not in aiMode:
+					aiMode.append(call.from_user.id)
 
-		markup = types.InlineKeyboardMarkup()
+				bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+				botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=ai_prompt_msg,reply_markup=back_markup)
+				users[call.from_user.id] = botmsg.message_id
+		elif call.data == "back":
+			markup = types.InlineKeyboardMarkup()
 
-		discord_button = types.InlineKeyboardButton(
-			text = "↪️ дискорд ссылка  ",
-			callback_data = "discord"
-		)
-		tg_button = types.InlineKeyboardButton(
-			text = "↪️ телеграм ссылка  ",
-			callback_data = "tg"
-		)
-		about_button = types.InlineKeyboardButton(
-			text = "❔ о нас  ",
-			callback_data = "about"
-		)
-		ai_button = types.InlineKeyboardButton(
-			text = "🗣 поговорить с сырный соус тян  ",
-			callback_data = "ai"
-		)
+			discord_button = types.InlineKeyboardButton(
+				text = "↪️ дискорд ссылка  ",
+				callback_data = "discord"
+			)
+			tg_button = types.InlineKeyboardButton(
+				text = "↪️ телеграм ссылка  ",
+				callback_data = "tg"
+			)
+			about_button = types.InlineKeyboardButton(
+				text = "❔ о нас  ",
+				callback_data = "about"
+			)
+			ai_button = types.InlineKeyboardButton(
+				text = "🗣 поговорить с сырный соус тян  ",
+				callback_data = "ai"
+			)
 
-		user_full = call.from_user.first_name
+			user_full = call.from_user.first_name
 
-		if call.from_user.last_name:
-			user_full += " " + call.from_user.last_name
+			if call.from_user.last_name:
+				user_full += " " + call.from_user.last_name
 
-		markup.add(discord_button)
-		markup.add(tg_button)
-		markup.add(about_button)
-		markup.add(ai_button)
+			markup.add(discord_button)
+			markup.add(tg_button)
+			markup.add(about_button)
+			markup.add(ai_button)
 
-		if call.from_user.id in users.keys():
-			botmsg = bot.edit_message_text(chat_id=call.from_user.id,text=f"👋 Приветствую {user_full}! " + start_msg,message_id=users[call.from_user.id],reply_markup=markup)
+			if call.from_user.id in users.keys():
+				with open("assets/starter.png","rb") as pic:
+					if call.from_user.id in aiMode:
+						aiMode.remove(call.from_user.id)
 
-			users[call.from_user.id] = botmsg.message_id
+					if call.from_user.id in inAiChat:
+						inAiChat.remove(call.from_user.id)
 
+						bot.delete_message(chat_id=call.from_user.id,message_id=users[call.from_user.id])
+						botmsg = bot.send_photo(chat_id=call.from_user.id,photo=pic,caption=start_msg,reply_markup=markup)
+						users[call.from_user.id] = botmsg.message_id
 
-			return
-		botmsg = bot.send_message(call.from_user.id,f"👋 Приветствую {user_full}! " + start_msg,reply_markup=markup)
+						return
+					else:
+						bot.edit_message_caption(chat_id=call.from_user.id,message_id=users[call.from_user.id],caption=f"👋 Приветствую {user_full}! " + start_msg,reply_markup=markup)
+						return
 
-		users[call.from_user.id] = botmsg.message_id
+			with open("assets/starter.png","rb") as pic:
+					botmsg = bot.send_photo(call.from_user.id,photo=pic,caption=f"👋 Приветствую {user_full}! " + start_msg,reply_markup=markup)
+					users[call.from_user.id] = botmsg.message_id
 
 @bot.message_handler(commands=['start'])
 def start(msg):
 	#if msg.from_user.id in users.keys():
 	#	return
+
+	if msg.from_user.id in aiMode:
+		aiMode.remove(msg.from_user.id)
+
+	if msg.from_user.id in inAiChat:
+		inAiChat.remove(msg.from_user.id)
 
 	markup = types.InlineKeyboardMarkup()
 
@@ -147,8 +234,11 @@ def start(msg):
 	markup.add(about_button)
 	markup.add(ai_button)
 
-	botmsg = bot.send_message(msg.from_user.id,f"👋 Приветствую {user_full}! " + start_msg,reply_markup=markup)
-	users[msg.from_user.id] = botmsg.message_id
+	with open("assets/starter.png","rb") as pic:
+		botmsg = bot.send_photo(msg.from_user.id,photo=pic,caption=f"👋 Приветствую {user_full}! " + start_msg,reply_markup=markup)
+		users[msg.from_user.id] = botmsg.message_id
+
+	bot.delete_message(chat_id=msg.from_user.id,message_id=msg.message_id)
 
 	print(users[msg.from_user.id])
 
@@ -158,21 +248,19 @@ def domsg(msg):
 		if msg.from_user.id in aiMode:
 			sleep = False
 
-			if datetime.now(timezone.utc).hour < 6 and datetime.now(timezone.utc).hour < 20:
-			#if datetime.now(timezone.utc).hour > 0:
+			if msg.from_user.id not in inAiChat:
+				inAiChat.append(msg.from_user.id)
+
+			if datetime.now(timezone.utc).hour+3 <= 9 and datetime.now(timezone.utc).hour+3 >= 21: # спит с 22:00 до 9:00 (UTC+3)
+			#if true:
 				if "разбудить сырный соус тян" in msg.text.lower():
-					#angry = all(word not in msg.text.lower() for word in badwords)
-
-					#for word in badwords:
-					#	if word in msg.text.lower():
-					#		angry = True
-					#		break
-
 					with open("assets/thinking_sleepy.png","rb") as pic:
-						bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id])
-						bot.edit_message_caption(chat_id=msg.from_user.id,message_id=users[msg.from_user.id],caption=thinking_msg)
+						bot.delete_message(chat_id=msg.from_user.id,message_id=users[msg.from_user.id])
+						botmsg = bot.send_photo(chat_id=msg.from_user.id,photo=pic,caption=thinking_msg,reply_markup=back_markup)
+						users[msg.from_user.id] = botmsg.message_id
 
 					angry = any(word in msg.text.lower() for word in badwords)
+
 					if angry:
 						response = getAIResponse(msg.text+sleepy_mask+angry_mask)
 					else:
@@ -181,72 +269,49 @@ def domsg(msg):
 					if angry:
 						with open("assets/angry_sleepy.png","rb") as pic:
 							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
-
-					if random.randint(1,2) == 1 and not angry:
-						with open("assets/sleepy.png","rb") as pic:
-							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
 					else:
-						if not angry:
-							with open("assets/sleepy_2.png","rb") as pic:
-								bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
+						with open(f"assets/sleepy_{random.randint(1,2)}.png","rb") as pic:
+							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
 				else:
-					sleep = True
-					response = sleep_mask
 					with open("assets/sleep.png","rb") as pic:
-						bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
+						bot.delete_message(chat_id=msg.from_user.id,message_id=users[msg.from_user.id])
+						botmsg = bot.send_photo(chat_id=msg.from_user.id, photo=pic,caption=sleep_mask,reply_markup=back_markup)
+						users[msg.from_user.id] = botmsg.message_id
+						return
 			else:
-				#angry = True
-
-				#for i in range(1,len(badwords)):
-				#	if badwords[i] in msg.text.lower():
-				#		angry = True
-				#		break
-
-				#for word in badwords:
-				#	if word in msg.text.lower():
-				#		angry = True
-				#		break
-
-				#angry = all(word not in msg.text.lower() for word in badwords)
 				with open("assets/thinking.png","rb") as pic:
-					bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id])
-					bot.edit_message_caption(chat_id=msg.from_user.id,message_id=users[msg.from_user.id],caption=thinking_msg)
+					bot.delete_message(chat_id=msg.from_user.id,message_id=users[msg.from_user.id])
+					botmsg = bot.send_photo(chat_id=msg.from_user.id,photo=pic,caption=thinking_msg,reply_markup=back_markup)
+					users[msg.from_user.id] = botmsg.message_id
 
 				angry = any(word in msg.text.lower() for word in badwords)
-
-				print(f"bad word detected: {angry}")
 
 				if angry:
 					response = getAIResponse(msg.text+angry_mask)
 				else:
 					response = getAIResponse(msg.text)
 
-				if random.randint(1,2) == 1:
-					if angry:
 
-						with open("assets/angry.png","rb") as pic:
-							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
-					else:
-						with open("assets/default.png","rb") as pic:
-							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
-				else:
-					if angry:
-						with open("assets/angry_2.png","rb") as pic:
-							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
-					else:
-						with open("assets/default_2.png","rb") as pic:
-							bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
+				rng = random.randint(1,2)
 
-			if sleep:
-				bot.edit_message_caption(chat_id=msg.from_user.id,message_id=users[msg.from_user.id],caption=response)
-			else:
-				bot.edit_message_caption(chat_id=msg.from_user.id,message_id=users[msg.from_user.id],caption=ai_yourmsg_msg+msg.text+"\n\n"+ai_answer_msg+response)
+				pic = open(f"assets/default_{rng}.png","rb")
+
+				if angry:
+					pic.close()
+					pic = open(f"assets/angry_{rng}.png","rb")
+
+				bot.edit_message_media(chat_id=msg.from_user.id, media=types.InputMediaPhoto(pic) , message_id=users[msg.from_user.id],reply_markup=back_markup)
+				pic.close()
+
+			bot.edit_message_caption(chat_id=msg.from_user.id,message_id=users[msg.from_user.id],caption=ai_yourmsg_msg+msg.text+"\n\n"+ai_answer_msg+response,reply_markup=back_markup)
 
 		elif msg.text.lower() == "томат":
-			bot.edit_message_text(chat_id=msg.from_user.id,text="""Вас инфицировали
+			bot.delete_message(chat_id=msg.from_user.id,message_id=users[msg.from_user.id])
+			botmsg = bot.send_message(chat_id=msg.from_user.id,text="""Вас инфицировали
 Теперь вы должны написать в описании пользователя "Напиши мне в ЛС слово Томат"
 Инфекция должна распространяться тихо
-Единственное правило - не спойлерить игру другим и не говорить заранее, что будет, если они напишут слово томат""",message_id=users[msg.from_user.id],reply_markup=back_markup)
+Единственное правило - не спойлерить игру другим и не говорить заранее, что будет, если они напишут слово томат""",reply_markup=back_markup)
+			users[msg.from_user.id] = botmsg.message_id
 	bot.delete_message(msg.from_user.id,msg.message_id)
 
 
